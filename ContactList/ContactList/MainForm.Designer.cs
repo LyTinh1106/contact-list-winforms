84c15f412d76acb6cddc158497ef0572ac1c5c0e
nhu <nguyenhuynhnhu4069@gmail.com>
2025-04-01 21:40:03 +0700

    xoa form lien he

--- ContactList/ContactList/MainForm.Designer.cs
+++ ContactList/ContactList/MainForm.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Location = new System.Drawing.Point(422, 262);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(84, 29);
+            this.button1.Size = new System.Drawing.Size(123, 58);
             this.button1.TabIndex = 0;
             this.button1.Text = "thêm liên hệ";
             this.button1.UseVisualStyleBackColor = true;
